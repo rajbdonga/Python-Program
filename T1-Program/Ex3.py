cae25dc32f343fe6d2a5b84a8ba0dd4068aaeb55
𝕽𝖆𝖏 𝕯𝖔𝖓𝖌𝖆⦖🍁 <bhupatbhaidonga8@gmail.com>
2024-10-10 08:42:38 +0530

    Add files via upload

---
+++ T1-Program/Ex3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+a=int(input("enter the value of a : "))
+b=int(input("enter the value of b : "))
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",(a//b))
+print(type(a))
+
+
+# In[8]:
+
+
+a=int(input("enter the value of a : "))
+b=int(input("enter the value of b : "))
+
+if(a>b):
+    print("a is greatter : ")
+else:
+    print("b is greatter : ")
+
+
+# In[ ]:
+
+
+
+
