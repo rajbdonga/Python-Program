150bae57354d0eaba15bb74f35bc01df8fb5cb73
𝕽𝖆𝖏 𝕯𝖔𝖓𝖌𝖆⦖🍁 <bhupatbhaidonga8@gmail.com>
2024-10-10 08:41:37 +0530

    Add files via upload

---
+++ T1-Program/Ex.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+radius = int(input("Enter value of radius : "))
+PI = 3.14
+print("area of circle :" , PI * radius * radius)
+print("perimeter of circle :" , 2 * PI * radius)
+
+
+# ![Operator-Precedence-and-Associativity-in-Python.jpg](attachment:Operator-Precedence-and-Associativity-in-Python.jpg)
